b92ab67e42bfbaa267892a3ba6dbeb81f6a8d183
CRISvsGAME <cris@crisvsgame.com>
2026-06-03 20:00:00 +0000

    Chroma 0.0.9
    Add RandomFloat Method

--- src/Chroma.ts
+++ src/Chroma.ts
@@ -2,4 +2,8 @@ export class Chroma {
     public static randomInt(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    public static randomFloat(min: number, max: number): number {
+        return Math.random() * (max - min) + min;
+    }
 }
