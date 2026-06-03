dce059aadd49f24569e24e71bc059decdaaf7638
CRISvsGAME <cris@crisvsgame.com>
2026-06-03 20:00:00 +0000

    Chroma 0.0.10
    Add RandomSRGB Method

--- src/Chroma.ts
+++ src/Chroma.ts
@@ -1,3 +1,9 @@
+export type Srgb = {
+    r: number;
+    g: number;
+    b: number;
+};
+
 export class Chroma {
     public static randomInt(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -5,5 +11,13 @@ export class Chroma {
 
     public static randomFloat(min: number, max: number): number {
         return Math.random() * (max - min) + min;
+    }
+
+    public static randomSrgb(): Srgb {
+        return {
+            r: this.randomInt(0, 255),
+            g: this.randomInt(0, 255),
+            b: this.randomInt(0, 255),
+        };
     }
 }
